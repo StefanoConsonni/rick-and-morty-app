ca9f64b79727e68d7ca11a5f3d9deffb4e72b70c
Stefano Consonni <stefanoconsonni@ymail.com>
2022-09-21 10:25:07 +0200

    add card styles

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,9 +1,21 @@
+import Character from "./Character";
+
+// styles
 import "./Card.css";
 
 export default function Card() {
 	return (
 		<div className="card">
-			<h1>Card</h1>
+			<img src={Character.image} alt={Character.name} />
+			<div className="card-info">
+				<p className="card-info__name">
+					<span>Name:</span>
+					{Character.name}
+				</p>
+				<p className="card-info__details">
+					{Character.species} <span>from</span> {Character.location.name}
+				</p>
+			</div>
 		</div>
 	);
 }
